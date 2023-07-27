d5627f94f09751952e080feff0ae57dbdbdbe19c
MashaBir22 <biryulyam@gmail.com>
2023-07-27 22:38:03 +0700

    New

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-public class BonusMilesService {
-    public int calculate(int cost) {
-        if (cost < 0) {
-            cost = 0;
-        }
-        return cost / 20;
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
-
 }
--- src/New.java
+++
@@ -1,8 +0,0 @@
-public class New.java {
-    public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
-        System.out.println(miles);
-    }
-}
